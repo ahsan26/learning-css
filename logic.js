efd3349af8145c0ca3c32befb1f158512529629f
ahsan26 <ahsanusmanabduljabbar@gmail.com>
2018-10-17 22:33:58 +0500

    added the horizontal slider files

--- logic.js
+++ logic.js
@@ -1,6 +1,8 @@
 var swiper = new Swiper('.swiper-container', {
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
     },
   });
